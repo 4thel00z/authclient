5ded285055b3fc73ef790509aa15235f8f721730
4thel00z <4thel00z@gmail.com>
2023-07-09 18:39:11 +0200

    [master] Add scope and multi-tenancy support
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- authclient/__init__.py
+++ authclient/__init__.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Tuple
 
 import httpx
@@ -11,20 +12,33 @@ class Token(BaseModel):
     id: str | None = None
 
 
+class Tenant(BaseModel):
+    id: uuid.UUID
+    name: str
+
+
+class Role(BaseModel):
+    id: uuid.UUID
+    name: str
+    scopes: list[str]
+
+
 class User(BaseModel):
+    id: uuid.UUID
     username: str
-    id: str | None = None
-    tenant: str | None = None
-    email: str | None = None
-    first_name: str | None = None
-    last_name: str | None = None
-    disabled: bool | None = None
+    email: str
+    first_name: str
+    last_name: str
+    password_hash: str
+    disabled: bool
+    tenant: Tenant
+    roles: list[Role]
 
 
-def get_token(base_url: str, username: str, password: str) -> Tuple[Token, Headers]:
+def get_token(base_url: str, username: str, password: str, tenant: str) -> Tuple[Token, Headers]:
     res = httpx.post(
-        f"{base_url}/token",
-        data={'username': username, 'password': password}
+        f"{base_url}/tokens",
+        data={'username': username, 'password': password, 'tenant': tenant}
     )
     res.raise_for_status()
     return Token(**res.json()), res.headers
@@ -36,7 +50,8 @@ def get_user(
 ) -> Tuple[User, Headers]:
     res = httpx.get(
         f"{base_url}/users/me",
-        headers={'Authorization': f"Bearer {token}"}
+        headers={'Authorization': f"Bearer {token}"},
+        follow_redirects=True,
     )
     res.raise_for_status()
     return User(**res.json()), res.headers
